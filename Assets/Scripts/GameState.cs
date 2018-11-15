8170a465b6f79cd49a7c505fa98101b52025575a
XtremeAI <40867886+XtremeAI@users.noreply.github.com>
2018-11-15 12:16:24 +0100

    GameState added

---
+++ Assets/Scripts/GameState.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameState : MonoBehaviour {
+
+	[Range(0.5f,10f)][SerializeField] float _gameSpeed = 1f;
+
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Time.timeScale = _gameSpeed;
+	}
+}
